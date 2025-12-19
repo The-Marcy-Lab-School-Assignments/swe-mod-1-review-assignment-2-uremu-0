4e46034f2eda1d92d916687ceb84f2b766d18492
uremu-0 <cjoftherose@gmail.com>
2025-12-18 22:26:14 -0500

    finished problem 3

--- src/3-reference-types.js
+++ src/3-reference-types.js
@@ -4,15 +4,22 @@ const team1 = {
   losses: 20
 };
 
-const team2 = team1;
-team2.wins = 50;
-team2.losses = 15;
 
-/* 
+/*
 Part A: What will team1.wins and team1.losses be after this code runs? Write your prediction as a comment below:
 
-- Prediction:
+- Prediction: team1.wins and team1.losses will be 50 and 15
 
 Part B: Fix the code so that team2 is a TRUE COPY of team1. After making your fix, changing team2 should NOT affect team1.
 Part C: Verify your fix by logging both team1.wins (should still be 45) and team2.wins (should be 50)
 */
+
+
+const team2 = structuredClone(team1)
+//const team2 = team1
+team2.wins = 50;
+team2.losses = 15;
+
+console.log(team1)
+
+
